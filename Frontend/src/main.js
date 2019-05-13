274baa48c0f036e6ea82139740ebe03dc58d99c0
Jayse-Ryu <dgkim723@hotmail.com>
2019-05-13 17:25:55 +0900

    Infomag-v2 / 13.May.2019 - endpoint updated

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -46,9 +46,9 @@ const store = new Vuex.Store({
     jwt: localStorage.getItem('token'),
     endpoints: {
       // TODO: Remove hardcoding of dev endpoints
-      obtainJWT: 'http://127.0.0.1:8000/api/api-token-auth/',
-      refreshJWT: 'http://127.0.0.1:8000/api/api-token-refresh/',
-      baseUrl: 'http://127.0.0.1:8000/api/'
+      obtainJWT: 'http://localhost/api/api-token-auth/',
+      refreshJWT: 'http://localhost/api/api-token-refresh/',
+      baseUrl: 'http://localhost/api/'
     },
     pageOptions: {
       landing: {
