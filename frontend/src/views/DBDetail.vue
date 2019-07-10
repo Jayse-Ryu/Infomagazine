c77e270303fe82a781ae906ffb92a216a2d52da3
Jayse-Ryu <dgkim723@hotmail.com>
2019-07-10 17:12:53 +0900

    Infomag-v2 / 10.July.2019 - DB detail page is set

--- frontend/src/views/DBDetail.vue
+++ frontend/src/views/DBDetail.vue
@@ -125,10 +125,32 @@
             </datepicker>
           </div>
           <div class="col-md-2">
-            <button type="button" class="btn btn-primary w-100">적용하기</button>
+            <button type="button" class="btn btn-info w-100" @click="date_clear()">초기화</button>
           </div>
           <div class="col-md-2">
-            <button type="button" class="btn btn-info w-100">엑셀저장</button>
+            <button type="button" class="btn btn-success w-100">엑셀저장</button>
+          </div>
+        </div>
+        <div class="row mt-3">
+          <div class="col-3">
+            <button type="button" class="btn btn-secondary w-100 p-0" @click="short_cut('recent3')">
+              최근3일
+            </button>
+          </div>
+          <div class="col-3">
+            <button type="button" class="btn btn-secondary w-100 p-0" @click="short_cut('recent2')">
+              최근2일
+            </button>
+          </div>
+          <div class="col-3">
+            <button type="button" class="btn btn-secondary w-100 p-0" @click="short_cut('yesterday')">
+              어제
+            </button>
+          </div>
+          <div class="col-3">
+            <button type="button" class="btn btn-secondary w-100 p-0" @click="short_cut('today')">
+              오늘
+            </button>
           </div>
         </div>
       </section>
@@ -144,23 +166,18 @@
             <div
               class="list-group-item text-center d-inline-flex justify-content-around p-1 pt-2 pb-2 text-center border-bottom-0"
               style="border-radius: 0; width:100%;">
-              <div class="p-0 w-100">업체</div>
-              <div class="p-0 w-100">페이지</div>
-              <div class="p-0 w-100">담당자</div>
-              <div class="p-0 w-100">조회수</div>
-              <div class="p-0 w-100">DB</div>
+              <div v-for="key in db_keys" :style="db_width" class="p-0">{{ key }}</div>
             </div>
           </div>
 
           <ul class="text-center list-group list-group-flush text-center">
             <li class="list-group-item list-group-item-action d-inline-flex justify-content-around p-1"
-                v-for="item in db_list">
-              <!--<div class="p-0 w-100">{{ landing_obj.company_id }}</div>
-              <div class="p-0 w-100">{{ landing_obj.landing_info.landing.name }}</div>
-              <div class="p-0 w-100">{{ landing_obj.landing_info.landing.manager }}</div>
-              <div class="p-0 w-100">{{ landing_obj.landing_info.views }}</div>
-              <div class="p-0 w-100">{{ landing_obj.landing_info.views }}</div>-->
-              <div>{{ item }}</div>
+                v-if="db_vals.length == 0">
+              <div class="p-0">데이터가 없습니다.</div>
+            </li>
+            <li class="list-group-item list-group-item-action d-inline-flex justify-content-around p-1"
+                v-else v-for="item in db_vals">
+              <div class="p-0" v-for="val in item" :style="db_width">{{ val }}</div>
             </li>
           </ul>
 
@@ -193,7 +210,7 @@
           연락처: '01088988898',
           성별: '남자',
           지역: '서울',
-          created_date: '1562231881156',
+          created_date: '1562728468',
           url: 'url_1'
         },
         {
@@ -202,17 +219,17 @@
           성별: '남자',
           지역: '서울',
           문의사항: '테스트가 되나요?',
-          created_date: '1562231281156',
+          created_date: '1562735504',
           url: 'url_1'
         },
         {
           이름: '고양이',
-          연락처: '01099000009',
+          연락처: null,
           성별: '수컷',
           지역: '집',
-          문의사항: 'ㅇ러293런ㄹㅇㄹㄴ어2',
+          문의사항: 'ㅇ러293런ㄹ어2',
           좋아: false,
-          created_date: '1562230181156',
+          created_date: '1394104654',
           url: 'url_1'
         },
         {
@@ -221,10 +238,12 @@
           성별: '암컷',
           지역: '우리집',
           좋아: true,
-          created_date: '1552231881156',
+          created_date: '1552231881',
           url: 'url_1'
         },
       ],
+      db_keys: [],
+      db_vals: [],
       start_time: '',
       finish_time: '',
       language: lang.ko
@@ -246,6 +265,7 @@
           .then((response) => {
             console.log(response)
             this.landing_obj = response.data.data
+            // Call get resources after data loaded
             this.get_resources()
           })
           .catch((error) => {
@@ -253,47 +273,114 @@
           })
       },
       get_resources() {
-
-        let key_flag = true
-
+        this.db_keys = []
+        this.db_vals = []
+        for (let index in this.db_list) {
+          // Extract db object by index
+          if (this.db_list.hasOwnProperty(index)) {
+            // Set db keys from one db object
+            for (let key in this.db_list[index]) {
+              // if db object has that extracted key
+              if (this.db_list[index].hasOwnProperty(key)) {
+                // if db_keys array not have this key yet, push. ot not.
+                if (!this.db_keys.includes(key)) {
+                  this.db_keys.push(key)
+                }
+              }
+            }
+            this.db_keys.splice(this.db_keys.indexOf('url'), 1)
+            this.db_keys.push('url')
+            this.db_keys.splice(this.db_keys.indexOf('created_date'), 1)
+            this.db_keys.push('created_date')
+          }
+        }
+        // Set values by key length
         for (let index in this.db_list) {
           if (this.db_list.hasOwnProperty(index)) {
-
-            console.log('db key', this.db_list[index])
-
-            for (let key in this.db_list[index]) {
-              if (this.db_list[index].hasOwnProperty(key)) {
-
-                console.log('index', index)
-                console.log('key', key)
-                console.log('val', this.db_list[index][key])
-                // if(key_flag) {
-                //   console.log('db key', this.db_list[index][key])
-                // } else {
-                //   console.log('db val', this.db_list[index][key])
-                // }
-                // key_flag = !key_flag
+            // value set by all of keys
+            let val_obj = []
+            let month_array = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+            let date_obj = ''
+            // Inspect by key name
+            for (let i = 0; i < this.db_keys.length; i++) {
+              // If this object has this key
+              if (this.db_list[index].hasOwnProperty(this.db_keys[i])) {
+                // If key is created date
+                if (this.db_keys[i] == 'created_date') {
+                  date_obj = ''
+                  // Epoch time string length must be 13
+                  if (this.db_list[index][this.db_keys[i]].length == 10) {
+                    date_obj = new Date(this.db_list[index][this.db_keys[i]] * 1000)
+                  } else {
+                    let double = Math.pow(10, this.db_list[index][this.db_keys[i]].length - 10)
+                    date_obj = new Date(this.db_list[index][this.db_keys[i]] * (1000 / double))
+                  }
+                  // Make date string
+                  let date_str = date_obj.getFullYear() + '-' + month_array[date_obj.getMonth()] + '-' + date_obj.getDate()
+                  val_obj.push(date_str)
+                } else {
+                  val_obj.push(this.db_list[index][this.db_keys[i]])
+                }
+              } else {
+                val_obj.push(' ')
               }
+            }
+            // If calendat works, Time comparison and push data or not
+            if (this.start_time != '' && this.finish_time != '') {
+              let from_t = new Date(this.start_time).setHours(0, 0, 0, 0)
+              let to_t = new Date(this.finish_time).setHours(0, 0, 0, 0)
+              let value_t = new Date(date_obj).setHours(0, 0, 0, 0)
+              if (from_t <= value_t && value_t <= to_t) {
+                this.db_vals.push(val_obj)
+              }
+            } else {
+              // It calendar is empty, push all data
+              this.db_vals.push(val_obj)
             }
           }
         }
-
+      },
+      date_clear() {
+        this.start_time = ''
+        this.finish_time = ''
+        this.get_resources()
+      },
+      short_cut(option) {
+        if (option == 'recent3') {
+          let from = new Date(new Date().setDate(new Date().getDate() - 3)).setHours(0, 0, 0, 0)
+          let to = new Date().setHours(0, 0, 0, 0)
+          this.start_time = new Date(from)
+          this.finish_time = new Date(to)
+          this.get_resources()
+        } else if (option == 'recent2') {
+          let from = new Date(new Date().setDate(new Date().getDate() - 2)).setHours(0, 0, 0, 0)
+          let to = new Date().setHours(0, 0, 0, 0)
+          this.start_time = new Date(from)
+          this.finish_time = new Date(to)
+          this.get_resources()
+        } else if (option == 'yesterday') {
+          let from = new Date(new Date().setDate(new Date().getDate() - 1)).setHours(0, 0, 0, 0)
+          let to = new Date(new Date().setDate(new Date().getDate() - 1)).setHours(0, 0, 0, 0)
+          this.start_time = new Date(from)
+          this.finish_time = new Date(to)
+          this.get_resources()
+        } else if (option == 'today') {
+          let from = new Date().setHours(0, 0, 0, 0)
+          let to = new Date().setHours(0, 0, 0, 0)
+          this.start_time = new Date(from)
+          this.finish_time = new Date(to)
+          this.get_resources()
+        }
       }
-      // set_data(option) {
-      //   if (option == 'from') {
-      //     console.log('from?')
-      //     this.calendar_range.from = this.start_time
-      //   } else if (option == 'to') {
-      //     console.log('to?')
-      //     this.calendar_range.to = this.finish_time
-      //   }
-      // }
     },
     computed: {
       range() {
         let range = {
           from: this.start_time,
           to: this.finish_time
+        }
+        if (range.from != '' && range.to != '') {
+          this.get_resources()
         }
         return range
       },
@@ -308,6 +395,10 @@
           to: this.start_time,
         }
         return range
+      },
+      db_width() {
+        let percent = 100 / this.db_keys.length
+        return 'width:' + percent + '%; word-break: break-all;'
       }
     }
   }
