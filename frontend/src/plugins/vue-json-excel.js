5c084af0cbf38263bfefbb99ee12f9f8ac406b61
Jayse-Ryu <dgkim723@hotmail.com>
2019-07-19 16:17:38 +0900

    Infomagazine-v2 / 19.July.2019 - Fix -> get back validations 2

--- frontend/src/plugins/vue-json-excel.js
+++ frontend/src/plugins/vue-json-excel.js
@@ -1,8 +1,4 @@
 import Vue from 'vue'
 import VueExcel from 'vue-json-excel'
-// import VueXlsx from 'xlsx'
 
-// Vue.use(Datepicker)
 Vue.component('vue-excel', VueExcel)
-// Vue.component('vue-xlsx', VueXlsx)
-// Vue.use(VueXlsx)
